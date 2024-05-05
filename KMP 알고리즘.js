9809d8ec4331abc939562ff1966082445c3f2138
팽이트위즈 <159697219+pangitwise@users.noreply.github.com>
2024-05-05 13:01:45 +0900

    ver. 1.0.0 KMP 알고리즘.js

---
+++ KMP 알고리즘.js
@@ -0,0 +1,39 @@
+
+// 문자열 검색 - KMP 알고리즘
+
+function fail(P) {
+    var j = -1;
+    var K = Array(P.length).fill(-1);
+    for (var i = 1; i < K.length; i++) {
+        j++;
+        K[i] = j;
+        if (P[j] !== P[i]) {
+            while (j !== -1 && P[i] !== P[j]) {
+                j = K[j];
+            }
+        }
+    }
+    return K;
+}
+
+function KMP(A, B) {
+    var match = 0;
+    for (var i = 0; i < A.length; i++) {
+        if (A[i] === B[match]) {
+            match += 1;
+        } else {
+            match = Math.max(0, K[match]);
+            if (match > 0) {
+                i--;
+            } else {
+                if (A[i] === B[0]) {
+                    match += 1;
+                }
+            }
+        }
+        if (match === B.length) {
+            return true;
+        }
+    }
+    return false;
+}
