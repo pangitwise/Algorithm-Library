cbfc79f761684d265c343fe86adbf08ce28e19cd
팽이트위즈 <159697219+pangitwise@users.noreply.github.com>
2024-05-05 12:42:58 +0900

    ver 1.0.1 유클리드 호제법.js
    확장 유클리드 호제법 알고리즘 추가

--- 유클리드 호제법.js
+++ 유클리드 호제법.js
@@ -45,3 +45,44 @@ function gcd(a, b) {
     if (r === 0) return b;
     return gcd(b, r);
 }
+
+
+// 확장 유클리드 호제법
+// - ax+by = N 꼴의 문제 풀기
+var x = [];
+var y = [];
+var R = [];
+
+function EEA(a, b, dep) {
+    var r = a%b;
+    var q = a/b;
+    q < 0 ? q = Math.ceil(q) : q = Math.floor(q);
+    if (r < 0) {
+        r += Math.abs(b);
+        b < 0 ? q += 1 : q -= 1;
+    }
+    // a = bq+r이므로, a-bq = r
+    if (dep === 0) {
+        var xi = 1;
+        var yi = -q;
+    // b = rq1+r1이므로, b = (a-bq)q1+r1
+    // a*(-q1) + b(1-q*q1) = r1
+    } else if (dep === 1) {
+        var xi = -q;
+        var yi = 1-y[dep-1]*q;
+    // 재귀적으로
+    // r_m = am+bm, r_n = an+bn이면 r_m = r_n*q+r_o
+    // am+bm = anq+bnq+r_o. a(m-nq)+b(m-nq) = r_o
+    } else {
+        var xi = x[dep-2]-x[dep-1]*q;
+        var yi = y[dep-2]-y[dep-1]*q;
+    }
+    x.push(xi);
+    y.push(yi);
+    R.push(r);
+    if (r === 1) {
+        candy1 = xi;
+    }
+    if (r === 0) return b;
+    return EEA(b, r, dep+1);
+}
